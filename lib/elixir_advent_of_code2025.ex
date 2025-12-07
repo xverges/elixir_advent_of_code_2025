3ab6045feb9ce64ed0aa5422628a53fb13ff958f
Xavier Vergés <xavier.verges@streamsets.com>
2025-12-07 15:49:15 +0100

    As created by mix new

---
+++ lib/elixir_advent_of_code2025.ex
@@ -0,0 +1,18 @@
+defmodule ElixirAdventOfCode2025 do
+  @moduledoc """
+  Documentation for `ElixirAdventOfCode2025`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirAdventOfCode2025.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_advent_of_code2025_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirAdventOfCode2025Test do
+  use ExUnit.Case
+  doctest ElixirAdventOfCode2025
+
+  test "greets the world" do
+    assert ElixirAdventOfCode2025.hello() == :world
+  end
+end
