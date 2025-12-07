175417e88da0527517ab757a6c2e8bfa7738bb2b
Xavier Vergés <xavier.verges@streamsets.com>
2025-12-07 21:19:48 +0100

    Day 1. Part 2. Failed attempt.

--- lib/day01.ex
+++ lib/day01.ex
@@ -3,10 +3,7 @@ defmodule AdventOfCode.Day01 do
   @spec solve_part1(binary()) :: non_neg_integer()
   def solve_part1(input) do
     origin = 50
-    moves = input
-            |> String.trim()
-            |> String.split("\n")
-            |> Enum.map(&parse_move/1)
+    moves = parse_moves(input)
     stops = Enum.scan(moves, origin, &apply_move/2)
     zeros = Enum.filter(stops, &(&1 == 0))
             |> length()
@@ -20,6 +17,31 @@ defmodule AdventOfCode.Day01 do
     |> solve_part1()
   end
 
+  @spec solve_part2(binary()) :: non_neg_integer()
+  def solve_part2(input) do
+    { origin, steps } = { 50, 0 }
+    moves = parse_moves(input)
+    stops = Enum.scan(moves, { origin, steps }, &track_move/2)
+    zeros = stops
+          |> Enum.map(fn {_, steps} -> steps end)
+          |> Enum.sum()
+    zeros
+  end
+
+  @spec solve_part2_from_file(String.t()) :: non_neg_integer()
+  def solve_part2_from_file(filename) do
+    filename
+    |> File.read!()
+    |> solve_part2()
+  end
+
+  defp parse_moves(input) do
+    input
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(&parse_move/1)
+  end
+
   defp parse_move("L" <> num), do: -String.to_integer(num)
   defp parse_move("R" <> num), do: String.to_integer(num)
 
@@ -29,5 +51,18 @@ defmodule AdventOfCode.Day01 do
     |> then(&(100 + &1))  # Add 100 to deal with negative numbers
     |> rem(100)
   end
+
+  defp count_full_laps(move), do: move |> div(100) |> abs()
+  defp count_partial_lap(0, 0), do: 0
+  defp count_partial_lap(remainder, origin) when (remainder + origin) == 0, do: 1
+  defp count_partial_lap(remainder, origin) when remainder < 0 and remainder + origin < 0, do: 1
+  defp count_partial_lap(remainder, origin) when remainder > 0 and remainder + origin > 100, do: 1
+  defp count_partial_lap(_remainder, _origin), do: 0
+
+  defp track_move(move, { origin, _ }) do
+    steps = count_full_laps(move) + count_partial_lap(rem(move, 100), origin)
+    { apply_move(move, origin), steps }
+  end
+
 
 end
--- test/day01_test.exs
+++ test/day01_test.exs
@@ -1,20 +1,37 @@
 defmodule AdventOfCode.Day01Test do
   use ExUnit.Case
   describe "AdventOfCode.Day01.part1" do
-    test "verify L50 R1 L1 steps twice in 0" do
+    test "verify L50 R1 L1 stops twice in 0" do
         input = "L50\nR1\nL1"
         password = 2
         assert AdventOfCode.Day01.solve_part1(input) == password
     end
-    test "verify R50 L1 R1 steps twice in 0" do
+    test "verify R50 L1 R1 stops twice in 0" do
         input = "R50\nL1\nR1"
         password = 2
         assert AdventOfCode.Day01.solve_part1(input) == password
     end
-    test "verify L250 L1 R1 steps twice in 0" do
+    test "verify L250 L1 R1 stops twice in 0" do
         input = "L250\nL1\nR1"
         password = 2
         assert AdventOfCode.Day01.solve_part1(input) == password
+    end
+  end
+  describe "AdventOfCode.Day01.part2" do
+    test "verify L50 R1 L2 steps twice over 0" do
+        input = "L50\nR1\nL2"
+        password = 2
+        assert AdventOfCode.Day01.solve_part2(input) == password
+    end
+    test "verify L250 L1 R1 steps four times in 0" do
+        input = "L250\nL1\nR1"
+        password = 4
+        assert AdventOfCode.Day01.solve_part2(input) == password
+    end
+    test "verify L50 L100 steps two times in 0" do
+        input = "L50\nL100"
+        password = 2
+        assert AdventOfCode.Day01.solve_part2(input) == password
     end
   end
 end
