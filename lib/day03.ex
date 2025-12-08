f86cf3dc3d3240485b51acf7b7ee3c2b7acea6da
Xavier Vergés <xavier.verges@streamsets.com>
2025-12-08 15:15:28 +0100

    Day 3. Part 1.

--- lib/day03.ex
+++ lib/day03.ex
@@ -2,6 +2,9 @@ defmodule AdventOfCode.Day03 do
   @spec solve_part1(binary()) :: non_neg_integer()
   def solve_part1(input) do
     input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&get_joltage/1)
+    |> Enum.sum()
   end
 
   @spec solve_part1_from_file(String.t()) :: non_neg_integer()
@@ -21,5 +24,23 @@ defmodule AdventOfCode.Day03 do
     filename
     |> File.read!()
     |> solve_part2()
+  end
+
+  @spec get_joltage(binary()) :: non_neg_integer()
+  def get_joltage(input) do
+    input
+    |> String.trim()
+    |> String.graphemes()
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.reduce({0, 0}, fn [a, b], joltage ->
+      {tens, units} = joltage
+      cond do
+        a > tens -> {a, b}
+        b > units -> {tens, b}
+        true -> {tens, units}
+      end
+    end)
+    |> then(fn {tens, units} -> tens * 10 + units end)
   end
 end
--- test/day03_test.exs
+++ test/day03_test.exs
@@ -2,18 +2,25 @@ defmodule AdventOfCode.Day03Test do
   use ExUnit.Case
 
   describe "AdventOfCode.Day03.part1" do
-    test "example input" do
+    test "example input results in example output" do
       input = """
-      # TODO: Add example input
-      """
+        987654321111111
+        811111111111119
+        234234234234278
+        818181911112111
+        """
+      expected = [98, 89, 78, 92]
+      inputs = input |> String.split("\n", trim: true)
 
-      expected = 0  # TODO: Add expected result
-      assert AdventOfCode.Day03.solve_part1(input) == expected
+      Enum.zip(inputs, expected)
+      |> Enum.each(fn {input, expected} ->
+        assert AdventOfCode.Day03.get_joltage(input) == expected
+      end)
+      assert AdventOfCode.Day03.solve_part1(input) == 357
     end
 
-    @tag :skip
     test "submitted solution works" do
-      assert AdventOfCode.Day03.solve_part1_from_file("inputs/day03.txt") == 0
+      assert AdventOfCode.Day03.solve_part1_from_file("inputs/day03.txt") == 17034
     end
   end
 
