cdb074fe0ac1fb5e8915c052ab61c0bf27d00509
Xavier Vergés <xavier.verges@streamsets.com>
2025-12-08 09:45:11 +0100

    Day 2. Part 2.

--- lib/day02.ex
+++ lib/day02.ex
@@ -15,6 +15,21 @@ defmodule AdventOfCode.Day02 do
     |> solve_part1()
   end
 
+  @spec solve_part2(binary()) :: non_neg_integer()
+  def solve_part2(input) do
+    input
+    |> parse_ranges()
+    |> get_invalid_ids(:part2)
+    |> Enum.sum()
+  end
+
+  @spec solve_part2_from_file(String.t()) :: non_neg_integer()
+  def solve_part2_from_file(filename) do
+    filename
+    |> File.read!()
+    |> solve_part2()
+  end
+
   @spec parse_ranges(binary()) :: [{integer(), integer()}]
   def parse_ranges(input) do
     transform = fn  txt_range ->
@@ -29,17 +44,17 @@ defmodule AdventOfCode.Day02 do
     |> Enum.map(transform)
   end
 
-  @spec get_invalid_ids([{integer(), integer()}]) :: [integer()]
-  def get_invalid_ids(ranges) do
-    Enum.flat_map(ranges, fn {first, last} -> get_invalid_ids_in_range(first, last) end)
+  @spec get_invalid_ids([{integer(), integer()}], atom()) :: [integer()]
+  def get_invalid_ids(ranges, strategy \\ :part1) do
+    Enum.flat_map(ranges, fn {first, last} -> get_invalid_ids_in_range(first, last, strategy) end)
   end
 
-  @spec get_invalid_ids_in_range(integer(), integer()) :: [integer()]
-  def get_invalid_ids_in_range(first, last) do
+  @spec get_invalid_ids_in_range(integer(), integer(), atom()) :: [integer()]
+  def get_invalid_ids_in_range(first, last, strategy \\ :part1) do
 
     invalid_ids = Stream.unfold(first, fn
       num when num <= last ->
-        next = next_candidate(num)
+        next = next_candidate(num, strategy)
         if num == next do
           {num, next + 1}
         else
@@ -61,7 +76,7 @@ defmodule AdventOfCode.Day02 do
     d = num_of_digits(num)
     "1" <> String.duplicate("0", d) |> String.to_integer()
   end
-  defp next_candidate(num) do
+  defp next_candidate(num, :part1) do
     lowest = if rem(num_of_digits(num), 2) == 0, do: num, else: first_with_extra_digit(num)
     num_digits = num_of_digits(lowest)
     candidate =
@@ -83,5 +98,49 @@ defmodule AdventOfCode.Day02 do
     else
       candidate
     end
+  end
+
+  defp next_candidate(num, :part2) do
+    num_digits = num_of_digits(num)
+
+    next_part2_candidate_with_length(num, num_digits) || next_part2_candidate_with_length(num, num_digits + 1)
+  end
+
+  defp next_part2_candidate_with_length(num, num_digits) do
+    # Size of the potentially repeated fragemnts
+    pattern_lengths = get_divisors(num_digits) |> Enum.filter(&(div(num_digits, &1) >= 2))
+
+    # For each fragment, find the smallest invalid ID >= num
+    pattern_lengths
+    |> Enum.map(fn pattern_length ->
+      repetitions = div(num_digits, pattern_length)
+      find_candidate_for_pattern(num, pattern_length, repetitions)
+    end)
+    |> Enum.reject(&is_nil/1)
+    |> Enum.min(fn -> nil end)
+  end
+
+  defp find_candidate_for_pattern(num, pattern_length, reps) do
+    # Find smallest number >= num that is `pattern` repeated `reps` times
+    # Extract pattern from num and potentially increment
+    num_str = to_string(num)
+    current_pattern_str = String.slice(num_str, 0, pattern_length)
+    candidate = String.duplicate(current_pattern_str, reps) |> String.to_integer()
+    current_pattern = String.to_integer(current_pattern_str)
+
+    min_pattern = if candidate >= num, do: current_pattern, else: current_pattern + 1
+    pattern = to_string(min_pattern)
+
+    # Have we gone past 9...9?
+    if String.length(pattern) != pattern_length do
+      nil
+    else
+      String.duplicate(pattern, reps) |> String.to_integer()
+    end
+  end
+
+  defp get_divisors(n) do
+    1..n
+    |> Enum.filter(&(rem(n, &1) == 0))
   end
 end
--- test/day02_test.exs
+++ test/day02_test.exs
@@ -22,4 +22,23 @@ defmodule AdventOfCode.Day02Test do
         assert AdventOfCode.Day02.solve_part1_from_file("inputs/day02.txt") == 38310256125
     end
   end
+  describe "AdventOfCode.Day02.part2" do
+    test "we get invalid ids from a range" do
+      assert AdventOfCode.Day02.get_invalid_ids_in_range(11, 22, :part2) == [11, 22]
+      assert AdventOfCode.Day02.get_invalid_ids_in_range(95, 115, :part2) == [99, 111]
+      assert AdventOfCode.Day02.get_invalid_ids_in_range(998, 1012, :part2) == [999, 1010]
+      assert AdventOfCode.Day02.get_invalid_ids_in_range(222220, 222224, :part2) == [222222]
+      assert AdventOfCode.Day02.get_invalid_ids_in_range(565653, 565659, :part2) == [565656]
+    end
+    test "verify provided example works" do
+        input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
+        ranges = AdventOfCode.Day02.parse_ranges(input)
+        expected = [11, 22, 99, 111, 999, 1010, 1188511885, 222222, 446446, 38593859, 565656, 824824824, 2121212121]
+        assert AdventOfCode.Day02.get_invalid_ids(ranges, :part2) == expected
+        assert AdventOfCode.Day02.solve_part2(input) == 4174379265
+    end
+  end
+    test "submitted solution works" do
+        assert AdventOfCode.Day02.solve_part2_from_file("inputs/day02.txt") == 58961152806
+    end
 end
